120b77e1891941447349988da0b46b5338d43247
Lin Yang <antelopehi@gmail.com>
2016-09-02 14:42:58 +0800

    第一版sass 頁面

---
+++ resources/views/page/sass/home-page-1.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{elixir('css/app.css')}}">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+<div class="wrap">
+    <div class="header">
+        <img src="/image/logo.jpeg">
+    </div>
+    <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem corporis dolorum, laudantium nam natus neque
+            nostrum odit. Architecto laborum temporibus ut. Accusamus velit veniam veritatis. Corporis dolore modi optio
+            vero.</p>
+        <p>Aliquam aliquid commodi consequuntur deserunt, iure labore molestiae placeat, porro provident ratione
+            repudiandae saepe temporibus veniam? Earum fuga harum ipsam laboriosam magni nam perspiciatis quibusdam
+            tempora vel veritatis? Commodi, vitae!</p>
+        <a href="#">dosesdf</a>
+        <lu>
+            <li>dfas</li>
+            <li>dfsa</li>
+            <li>sdfsa</li>
+        </lu>
+    </div>
+    <div class="footer">
+        <span>ADD:sdfsdfsdksladfldsflskdlffs</span>
+    </div>
+</div>
+
+</body>
+</html>
